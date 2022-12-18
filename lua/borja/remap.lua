4309909078502687566d66ad54db441a6a4e7ea6
Jesus Borja <yisus.bh25@gmail.com>
2022-12-17 21:46:48 -0600

    Add some helpfull remaps
    Remap to save the current file and Remap to save and open a terminal

--- lua/borja/remap.lua
+++ lua/borja/remap.lua
@@ -1,8 +1,16 @@
+-- leader key
 vim.g.mapleader = " "
+
+-- open netrw (file view)
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
--- move lines when highlighted
+-- open netrw (file view)
+vim.keymap.set("n", "<leader>w", vim.cmd.w)
 
+-- saves the file and opens a terminal
+vim.keymap.set("n", "<leader>tr", ":w <CR> :ter<CR>")
+
+-- move lines when highlighted
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
