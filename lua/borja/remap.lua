253a6ad46e6b4a3d6888683846e34639ffb623d0
Jesus Borja <yisus.bh25@gmail.com>
2022-12-19 16:07:31 -0600

    Add more remaps
    Remap to open terminal Remaps to center screen when scrolling

--- lua/borja/remap.lua
+++ lua/borja/remap.lua
@@ -10,8 +10,16 @@ vim.keymap.set("n", "<leader>w", vim.cmd.w)
 -- saves the file and opens a terminal
 vim.keymap.set("n", "<leader>tr", ":w <CR> :ter<CR>")
 
+-- open a terminal
+vim.keymap.set("n", "<leader>T", ":ter<CR>")
+
 -- move lines when highlighted
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
+
+-- centers the cursor when moving half a page down or up
+-- zz centers the cursor
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
